cc6e045bed4e04c424f17cdae2a6e4267c6b35c8
maw101 <maw101@aber.ac.uk>
2020-05-26 15:45:01 +0100

    Defined new item initialisation function

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,2 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "hash_table.h"
 
+// define initialisation function
+static hash_table_item * hash_table_new_item(const char * k, const char * v) {
+    hash_table_item * item = malloc(sizeof(hash_table_item)); // TODO: check malloc response
+
+    item->key = strdup(k); // duplicate key
+    item->value = strdup(v); // duplicate value
+
+    return item;
+}
