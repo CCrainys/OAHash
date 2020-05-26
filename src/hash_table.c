0e02e81056916b6e618008ee201a889cd2315a60
maw101 <maw101@aber.ac.uk>
2020-05-26 15:48:07 +0100

    Defined table initialisation function

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,7 +3,7 @@
 
 #include "hash_table.h"
 
-// define initialisation function
+// define item initialisation function
 static hash_table_item * hash_table_new_item(const char * k, const char * v) {
     hash_table_item * item = malloc(sizeof(hash_table_item)); // TODO: check malloc response
 
@@ -11,4 +11,15 @@ static hash_table_item * hash_table_new_item(const char * k, const char * v) {
     item->value = strdup(v); // duplicate value
 
     return item;
+}
+
+// define table initialisation function
+hash_table_table * hash_table_new() {
+    hash_table_table * hash_table = malloc(sizeof(hash_table_table)); // TODO: check malloc response
+
+    hash_table->count = 0;
+    hash_table->items = calloc((size_t) hash_table->size, sizeof(hash_table_item *)); // TODO: check calloc response
+    hash_table->size = 50; // TODO: add resize ability in future
+
+    return hash_table;
 }
