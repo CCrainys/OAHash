2f2d5dec0327ebbad4d515d8f05d4ffdfccfd6d3
maw101 <maw101@aber.ac.uk>
2020-05-26 15:40:09 +0100

    Defined item struct for K-V pairs

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,4 +1,12 @@
 #ifndef SRC_HASH_TABLE_H
 #define SRC_HASH_TABLE_H
 
+// define item struct for key-value pairs
+typedef struct {
+    char * key;
+    char * value;
+} hash_table_item;
+
+
+
 #endif //SRC_HASH_TABLE_H
