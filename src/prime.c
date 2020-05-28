ca6e22d33e90d3b53e4d4c953c47dbc2d8dc984b
maw101 <maw101@aber.ac.uk>
2020-05-28 12:00:38 +0100

    Implemented prime functions

--- src/prime.c
+++ src/prime.c
@@ -3,9 +3,18 @@
 #include "prime.h"
 
 int is_prime(const int x) {
-    return 0; // TODO: implement
+    if (x < 2) return -1;
+    if (x < 4) return 1;
+    if ((x % 2) == 0) return 0;
+    for (int i = 3; i <= floor(sqrt((double) x)); i += 2) {
+        if ((x % i) == 0) return 0;
+    }
+    return 1;
 }
 
 int get_next_prime(int x) {
-    return 0; // TODO: implement
+    while (is_prime(x) != 1) {
+        x++;
+    }
+    return x;
 }
