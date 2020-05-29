4615d52bf6d45487cdfebf9fbc7b0beac2000fda
maw101 <maw101@aber.ac.uk>
2020-05-29 12:45:36 +0100

    Renamed function prototypes for grow/shrink in header

--- src/hash_table.h
+++ src/hash_table.h
@@ -29,7 +29,7 @@ static int hash_table_dh_get_hash(const char * s, const int hash_table_size, con
 void hash_table_insert(hash_table_table * hash_table, const char * key, const char * value);
 char * hash_table_search(hash_table_table * hash_table, const char * key);
 
-static void hash_table_resize_upwards(hash_table_table * hash_table);
-static void hash_table_resize_downwards(hash_table_table * hash_table);
+static void hash_table_resize_grow(hash_table_table * hash_table);
+static void hash_table_resize_shrink(hash_table_table * hash_table);
 
 #endif //SRC_HASH_TABLE_H
